f11f6424422a215bfa95c177eb639c9dd326cd43
Hugh Pearse <hughpear@localhost.localdomain>
2016-09-19 10:42:46 +0100

    fixed returning tuples

--- server-mapreduce-des-ctr-mode-example.py
+++ server-mapreduce-des-ctr-mode-example.py
@@ -15,30 +15,32 @@ def chunks(longdata, n):
     for i in range(0, len(longdata), n):
         yield longdata[i:i + n]
 
-plain_text = 'AAAABBBBCCCC'
+plain_text = 'AAAABBBBCCCCDDDDAAAABBBBCCCC'
 plain_text = addPadding(plain_text)
 datasource = dict(enumerate(list(chunks(plain_text, 8)), start=1))
 
 def mapfn(k, v):
-    for w in v.split():
-        yield w, k
+    yield v, k
 
 def reducefn(k, vs):
     from Crypto.Cipher import DES
     from Crypto import Random
     from Crypto.Util import Counter
     import struct
+    result = []
     key = "12345678"
     #nonce = Random.new().read(DES.block_size/2)
     #ctr = Counter.new(DES.block_size*8/2, prefix=nonce)
     nonce = "0000"
-    ctr = nonce + struct.pack("!I",vs[0])
-    def getctr():
-        print k + ", " + str(ctr)
-        return str(ctr)
-    des = DES.new(key, DES.MODE_CTR, counter=getctr)
-    cipher_text = des.encrypt(k)
-    return cipher_text
+    for offsetval in vs:
+        ctr = nonce + struct.pack("!I",offsetval)
+        def getctr():
+            return str(ctr)
+        des = DES.new(key, DES.MODE_CTR, counter=getctr)
+        cipher_text = des.encrypt(k)
+        result += (cipher_text,offsetval)
+    #print "k: " + k + ", vs: " + str(vs) + ", ctr: " + ctr + "cipher_text: " + cipher_text
+    return result
 
 s = mincemeat.Server()
 s.datasource = datasource
@@ -46,6 +48,6 @@ s.mapfn = mapfn
 s.reducefn = reducefn
 
 results = s.run_server(password="changeme")
-#print results
-print "ciphertext (base 16 encoded): " + str(map(''.join, zip(*[iter(base64.b16encode(''.join(results.values())))]*16)))
+print results
+#print "ciphertext (base 16 encoded): " + str(map(''.join, zip(*[iter(base64.b16encode(''.join(results.values())))]*16)))
 
